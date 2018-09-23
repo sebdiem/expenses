d40dfb8edaf0380e50f7a11bc2c6ac7728643fdc
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-09-23 19:10:27 +0200

    update sw

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,8 +1,11 @@
-const version = '1';
+const appName = 'expenses';
+const version = '2';
+
+const expectedCaches = [`{appName}-static-${version}`];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(`static-${version}`)
+    caches.open(`${appName}-static-${version}`)
       .then(cache => cache.addAll([
         '/',
         '/app.js',
